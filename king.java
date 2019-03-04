f5a0549f9302858856fed0fcdec6f6693c8ccb73
Athi_Nair_2000 <athinair2000@gmail.com>
2019-03-04 23:55:13 +0530

    Solution to king's race in java

---
+++ king.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class king{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int x=sc.nextInt();
+		int y=sc.nextInt();	
+
+	
+	
+	if(x+y<n+2){
+		System.out.println("white");
+	}
+	else{
+		System.out.println("black");
+	}
+	
+	}
+}
+
